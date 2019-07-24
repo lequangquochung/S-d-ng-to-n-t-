cd5175cd6fca3a485d0fa9166afe95a12ec91ea5
Hung Le <=>
2019-07-24 11:17:59 +0700

    add 2 files html

---
+++ myjs.js
@@ -0,0 +1,6 @@
+var width;
+var height;
+width = parseInt(prompt('Enter the width'));
+height = parseInt(prompt('Enter the height'));
+var area = width * height;
+document.getElementById('area').innerHTML = area;
